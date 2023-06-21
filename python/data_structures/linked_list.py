0d8962efc3705056ec2b964c09f149518a6c721c
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-06-21 17:39:32 -0400

    code challenge 07 done

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -33,7 +33,22 @@ class LinkedList:
             current = current._next
         return False
 
-
+     #kth from end
+  #argument: a number, k, as a parameter.
+  #Return the node's value that is k places from the tail of the linked list.
+  #You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+    def kth_from_end(self, k):
+        current = self.head
+        count = 0
+        while current:
+          count += 1
+          current = current._next
+        if k < 0 or k >= count:
+          raise TargetError("Target not found")
+        current = self.head
+        for i in range(count - k - 1):
+          current = current._next
+        return current.value
 
     def traverse(self):
         current = self.head
@@ -42,8 +57,11 @@ class LinkedList:
             current = current._next
 
 
-class TargetError:
+# Putting in exception here fixes the error:
+#TypeError: expected exception must be a BaseException type, not TargetError
+class TargetError(Exception):
     pass
+
 
 
 if __name__ == '__main__':
