0b92065e97e3aaf71b129d5ee8af626f599927f8
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-06-28 21:05:00 -0400

    Code challenge update

---
+++ python/401CodeChallenge/CodeChallenge13/READEME.md
@@ -0,0 +1,22 @@
+https://geekflare.com/valid-parentheses-checking-python/
+
+
+Collaborators
+Andrew Carroll
+Anthony Sinitsa
+Logan Reese
+Sarah Glass
+
+# Challenge Title
+Code Challenge: Class 12
+
+## Whiteboard Process
+![CodeChallenge07](<Screenshot 2023-06-28 210331.png>)
+
+## Approach & Efficiency
+Time: o(n)
+Space: o(n)
+
+## Solution
+run pytest
+python/code_challenges/stack_queue_animal_shelter.py
--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,54 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
+
+def multi_bracket_validation(str):
+    stack = []
+    left_brackets = ['(','[','{']
+    right_brackets = [')',']','}']
 
 
-def multi_bracket_validation():
-    pass
+    for char in str:
+        if char in left_brackets:
+            stack.append(char)
+        elif char in right_brackets:
+            if not stack:
+                return False
+
+
+            top = stack.pop()
+            opening_index = left_brackets.index(top)
+            closing_index = right_brackets.index(char)
+            if opening_index != closing_index:
+
+                return False
+
+    return len(stack) == 0
+
+# def multi_bracket_validation():
+#     pass
+
+#Iterate over left/right brackets
+#Check if stack is empty, if not then continue
+#If stack is empty then return false
+
+# def multi_bracket_validation(str):
+#     stack = []
+#     opening_brackets = ['(','[','{']
+#     closing_brackets = [')',']','}']
+
+
+#     for char in str:
+#         if char in opening_brackets:
+#             stack.append(char)
+#         elif char in closing_brackets:
+#             if not stack:
+#                 return False
+
+
+#             top = stack.pop()
+#             opening_index = opening_brackets.index(top)
+#             closing_index = closing_brackets.index(char)
+#             if opening_index != closing_index:
+
+#                 return False
+
+#     return len(stack) == 0
