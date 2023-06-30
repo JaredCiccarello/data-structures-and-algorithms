acc32e044503edb2c6fc07b1069f63aeeca07890
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-06-30 18:14:34 -0400

    Code Challenge 15 complete

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -2,51 +2,45 @@ from data_structures.stack import Stack
 
 def multi_bracket_validation(str):
     stack = []
-    left_brackets = ['(','[','{']
-    right_brackets = [')',']','}']
-
+    left_brackets = ['[','{','('] #'({['
+    right_brackets = [']','}',')'] #')}]'
 
     for char in str:
         if char in left_brackets:
             stack.append(char)
         elif char in right_brackets:
-            if not stack:
+            if not Stack:
                 return False
-
 
             top = stack.pop()
-            opening_index = left_brackets.index(top)
-            closing_index = right_brackets.index(char)
-            if opening_index != closing_index:
+            left_brackets = left_brackets.index(top)
+            right_brackets = right_brackets.index(char)
+            if left_brackets != right_brackets:
+            # if (char == ']' and top != '[') or (char == '}' and top != '{') or (char == ')' and top != '('):
 
                 return False
-
     return len(stack) == 0
 
 # def multi_bracket_validation():
 #     pass
 
-#Iterate over left/right brackets
-#Check if stack is empty, if not then continue
-#If stack is empty then return false
-
 # def multi_bracket_validation(str):
 #     stack = []
-#     opening_brackets = ['(','[','{']
-#     closing_brackets = [')',']','}']
+#     left_brackets = ['(','[','{']
+#     right_brackets = [')',']','}']
 
 
 #     for char in str:
-#         if char in opening_brackets:
+#         if char in left_brackets:
 #             stack.append(char)
-#         elif char in closing_brackets:
+#         elif char in right_brackets:
 #             if not stack:
 #                 return False
 
 
 #             top = stack.pop()
-#             opening_index = opening_brackets.index(top)
-#             closing_index = closing_brackets.index(char)
+#             opening_index = left_brackets.index(top)
+#             closing_index = right_brackets.index(char)
 #             if opening_index != closing_index:
 
 #                 return False
--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,6 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import Node
+
 
 
 class BinarySearchTree(BinaryTree):
@@ -8,7 +10,54 @@ class BinarySearchTree(BinaryTree):
 
     def __init__(self):
         # initialization here
-        pass
+        super().__init__()
+        self.root = None
+        self.Node = Node
+
+    def add(self, value):
+      """
+        Adds a new node with the given value to the binary search tree.
+        """
+      if self.root is None:
+            self.root = Node(value)
+      else:
+            self._add_recursive(self.root, value)
+
+    def _add_recursive(self, node, value):
+        """
+        Helper method for recursive insertion of a node.
+        """
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._add_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._add_recursive(node.right, value)
+
+    def contains(self, value):
+        """
+        Checks if the binary search tree contains a node with the given value.
+        Returns True if found, False otherwise.
+        """
+        return self._contains_recursive(self.root, value)
+
+    def _contains_recursive(self, node, value):
+        """
+        Helper method for recursive search of a value in the binary search tree.
+        """
+        if node is None:
+            return False
+        if value == node.value:
+            return True
+        if value < node.value:
+            return self._contains_recursive(node.left, value)
+        else:
+            return self._contains_recursive(node.right, value)
+
 
     def some_method(self):
         # method body here
