0d77031d8edb3564141bffe97ba835db0cc36769
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-07-08 14:49:41 -0400

    Completed rework

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,49 +1,47 @@
 from data_structures.stack import Stack
 
-def multi_bracket_validation(bracketString):
-    #Setting a variable of stack and then create an instance of the imported class of Stack.
-    stack = Stack()
-    left_brackets = ['[','{','('] #'({['
-    right_brackets = [']','}',')'] #')}]'
 
-    for char in bracketString:
-        if char in left_brackets:
+def multi_bracket_validation(bracket_string):
+  #create instance of stack used for storing brackets
+  stack = Stack()
+  # left_brackets = ['(', '[', '{']
+  # right_brackets = [')', ']', '}']
+
+  #create an index of brackets
+  bracket_index = {
+      #Right, left, right, left, kind of like marching
+        ')':'(',
+        ']':'[',
+        '}':'{'
+    }
+
+  for char in bracket_string:
+        if char in bracket_index.values():
             stack.push(char)
-        elif char in right_brackets:
-            if not stack:
+        elif char in bracket_index.keys():
+            if stack.is_empty() or stack.pop() != bracket_index[char]:
                 return False
 
-            top = stack.pop()
-            left_brackets = left_brackets.index(top)
-            right_brackets = right_brackets.index(char)
-            if left_brackets != right_brackets:
-              if (char == ']' and top != '[') or (char == '}' and top != '{') or (char == ')' and top != '('):
+  return stack.is_empty()
 
-                return False
-    return stack.is_empty() == True
+# def multi_bracket_validation(bracketString):
+    #Setting a variable of stack and then create an instance of the imported class of Stack.
 
-# def multi_bracket_validation():
-#     pass
+    # stack = Stack()
+    # left_brackets = ['[','{','('] #'({['
+    # right_brackets = [']','}',')'] #')}]'
 
-# def multi_bracket_validation(str):
-#     stack = []
-#     left_brackets = ['(','[','{']
-#     right_brackets = [')',']','}']
+    # for char in bracketString:
+    #     if char in left_brackets:
+    #         stack.push(char)
+    #     elif char in right_brackets:
+    #         if stack.is_empty:
+    #             return False
 
+    #         top = stack.pop()
+    #         left_brackets_index = left_brackets.index(top)
+    #         right_brackets_index = right_brackets.index(char)
+    #         if left_brackets_index != right_brackets_index:
+    #             return False
 
-#     for char in str:
-#         if char in left_brackets:
-#             stack.append(char)
-#         elif char in right_brackets:
-#             if not stack:
-#                 return False
-
-
-#             top = stack.pop()
-#             opening_index = left_brackets.index(top)
-#             closing_index = right_brackets.index(char)
-#             if opening_index != closing_index:
-
-#                 return False
-
-#     return len(stack) == 0
+    # return stack.is_empty()
