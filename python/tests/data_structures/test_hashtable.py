937c9c353ef07ebdaeafd766124fb2f4f5394af5
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-07-21 19:58:43 -0400

    Not even close

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,7 +6,7 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_get_apple():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
@@ -15,7 +15,7 @@ def test_get_apple():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_internals():
     hashtable = Hashtable(1024)
     hashtable.set("ahmad", 30)
@@ -32,3 +32,60 @@ def test_internals():
     expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
 
     assert actual == expected
+
+@pytest.mark.skip("TODO")
+def test_set():
+    hashtable = Hashtable(size=5)
+    hashtable.set("apple", 5)
+    hashtable.set("banana", 3)
+    hashtable.set("orange", 2)
+
+    assert hashtable._buckets[0].value == 5
+    assert hashtable._buckets[3].value == 3
+    assert hashtable._buckets[2].value == 2
+
+@pytest.mark.skip("TODO")
+def test_get():
+    hashtable = Hashtable(size=5)
+    hashtable.set("apple", 5)
+    hashtable.set("banana", 3)
+    hashtable.set("orange", 2)
+
+    assert hashtable.get("apple") == 5
+    assert hashtable.get("banana") == 3
+    assert hashtable.get("orange") == 2
+
+@pytest.mark.skip("TODO")
+def test_has():
+    hashtable = Hashtable(size=5)
+    hashtable.set("apple", 5)
+    hashtable.set("banana", 3)
+    hashtable.set("orange", 2)
+
+    assert hashtable.has("apple") is True
+    assert hashtable.has("banana") is True
+    assert hashtable.has("watermelon") is False
+
+@pytest.mark.skip("TODO")
+def test_keys():
+    hashtable = Hashtable(size=5)
+    hashtable.set("apple", 5)
+    hashtable.set("banana", 3)
+    hashtable.set("orange", 2)
+
+    assert hashtable.keys() == ["apple", "banana", "orange"]
+
+@pytest.mark.skip("TODO")
+def test_non_existing_key():
+    hashtable = Hashtable(size=5)
+    hashtable.set("apple", 5)
+    hashtable.set("banana", 3)
+
+    try:
+        hashtable.get("grape")
+    except KeyError:
+        pass
+    else:
+        assert False, "Expected KeyError but no exception was raised."
+
+
