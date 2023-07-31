09ee30a64ad1fa6101d0bd35e7214848512cb5a4
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-07-31 16:45:40 -0400

    not quite

--- sorting/comparisons/test_comparison.py
+++ sorting/comparisons/test_comparison.py
@@ -1,86 +1,82 @@
 import pytest
-import sys
-import os
-
-# Now you can import the sorting module
 from sorting.comparisons.comparison_sort import sort_movies_by_most_recent_year, sort_movies_alphabetically_by_title
 
-def test_exist():
-    assert sort_movies_alphabetically_by_title()
+# Sample movie data
+@pytest.fixture
+def movies():
+    return [
+        {
+            'title': 'The Great Gatsby',
+            'year': 2013,
+            'genres': ['Drama', 'Romance']
+        },
+        {
+            'title': 'An Inconvenient Truth',
+            'year': 2006,
+            'genres': ['Documentary']
+        },
+        {
+            'title': 'Memento',
+            'year': 2000,
+            'genres': ['Mystery', 'Thriller']
+        },
+        {
+            'title': 'Avatar',
+            'year': 2009,
+            'genres': ['Action', 'Adventure', 'Fantasy']
+        }
+    ]
 
-def setUp(self):
-        # Sample movie data
-        self.movies = [
-            {
-                'title': 'The Great Gatsby',
-                'year': 2013,
-                'genres': ['Drama', 'Romance']
-            },
-            {
-                'title': 'An Inconvenient Truth',
-                'year': 2006,
-                'genres': ['Documentary']
-            },
-            {
-                'title': 'Memento',
-                'year': 2000,
-                'genres': ['Mystery', 'Thriller']
-            },
-            {
-                'title': 'Avatar',
-                'year': 2009,
-                'genres': ['Action', 'Adventure', 'Fantasy']
-            }
-        ]
+@pytest.mark.test
+def test_sort_by_most_recent_year(movies):
+    sorted_movies = sort_movies_by_most_recent_year(movies)
+    expected_output = [
+        {
+            'title': 'The Great Gatsby',
+            'year': 2013,
+            'genres': ['Drama', 'Romance']
+        },
+        {
+            'title': 'Avatar',
+            'year': 2009,
+            'genres': ['Action', 'Adventure', 'Fantasy']
+        },
+        {
+            'title': 'An Inconvenient Truth',
+            'year': 2006,
+            'genres': ['Documentary']
+        },
+        {
+            'title': 'Memento',
+            'year': 2000,
+            'genres': ['Mystery', 'Thriller']
+        }
+    ]
+    assert sorted_movies == expected_output
 
-def test_sort_by_most_recent_year(self):
-        sorted_movies = sort_movies_by_most_recent_year(self.movies)
-        expected_output = [
-            {
-                'title': 'The Great Gatsby',
-                'year': 2013,
-                'genres': ['Drama', 'Romance']
-            },
-            {
-                'title': 'Avatar',
-                'year': 2009,
-                'genres': ['Action', 'Adventure', 'Fantasy']
-            },
-            {
-                'title': 'An Inconvenient Truth',
-                'year': 2006,
-                'genres': ['Documentary']
-            },
-            {
-                'title': 'Memento',
-                'year': 2000,
-                'genres': ['Mystery', 'Thriller']
-            }
-        ]
-        self.assertEqual(sorted_movies, expected_output)
-
-def test_sort_alphabetically_by_title(self):
-        sorted_movies = sort_movies_alphabetically_by_title(self.movies)
-        expected_output = [
-            {
-                'title': 'Avatar',
-                'year': 2009,
-                'genres': ['Action', 'Adventure', 'Fantasy']
-            },
-            {
-                'title': 'The Great Gatsby',
-                'year': 2013,
-                'genres': ['Drama', 'Romance']
-            },
-            {
-                'title': 'Memento',
-                'year': 2000,
-                'genres': ['Mystery', 'Thriller']
-            },
-            {
-                'title': 'An Inconvenient Truth',
-                'year': 2006,
-                'genres': ['Documentary']
-            }
-        ]
-        self.assertEqual(sorted_movies, expected_output)
+@pytest.mark.test
+def test_sort_alphabetically_by_title(movies):
+    sorted_movies = sort_movies_alphabetically_by_title(movies)
+    expected_output = [
+        {
+            'title': 'Avatar',
+            'year': 2009,
+            'genres': ['Action', 'Adventure', 'Fantasy']
+        },
+        {
+            'title': 'The Great Gatsby',
+            'year': 2013,
+            'genres': ['Drama', 'Romance']
+        },
+        {
+            'title': 'Memento',
+            'year': 2000,
+            'genres': ['Mystery', 'Thriller']
+        },
+        {
+            'title': 'An Inconvenient Truth',
+            'year': 2006,
+            'genres': ['Documentary']
+        }
+    ]
+    assert sorted_movies == expected_output
